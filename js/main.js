f4f50d5b2320b017b791eb630b83373f5f7d40e5
Ray Otsuka <56874556+ray-o-0509@users.noreply.github.com>
2022-08-02 00:52:42 +0900

    [add] update

--- js/main.js
+++ js/main.js
@@ -11,3 +11,18 @@ function portal_clicked() {
     }
 
 }
+
+const mahoujin_answers = ['8', '5', '7', '4', '20', '3', '2', '9'];
+
+function check_answer() {
+    for (let index = 0; index < mahoujin_answers.length; index++) {
+        let data = $('.input_' + index).val();
+        console.log(data);
+        if (data != mahoujin_answers[index]) {
+            alert('答えが間違っています。※半角で入力してない場合も表示されます。');
+            return;
+        }
+    }
+    alert('正解！');
+    location.href = 'index.html';
+}
